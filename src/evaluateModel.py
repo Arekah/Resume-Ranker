ccf0c2b98add7309ef333ed9528dea75e5a072e6
Ziyad Khan <Ziyadkhan2333@gmail.com>
2025-05-31 15:52:57 +0500

    removed print statements

--- src/evaluateModel.py
+++ src/evaluateModel.py
@@ -3,10 +3,6 @@ from sklearn import metrics
 def EvaluateModel(clf , X_train , y_train , X_test , y_test):
     prediction = clf.predict(X_test)
 
-    print('Accuracy of KNeighbors Classifier on training set: {:.2f}'.format(clf.score(X_train, y_train)))
-    print('Accuracy of KNeighbors Classifier on test set: {:.2f}'.format(clf.score(X_test, y_test)))
-
-    print("\n Classification report for classifier %s:\n%s\n" % (clf, metrics.classification_report(y_test, prediction))) 
 
     return prediction;
     
